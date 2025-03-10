00bb658446fcc6dda157b77eb268279323e1dd77
FAUCHET Malo <malofauchetpro@gmail.com>
2025-03-10 09:39:46 +0100

    Class Language + Currency

---
+++ html/data/class_currency.js
@@ -0,0 +1,60 @@
+"use strict";
+
+class Currency {
+    static all_currencies = {};
+
+    constructor(code, name, symbol) {
+        this._code = code;
+        this._name = name;
+        this._symbol = symbol;
+    }
+
+    toString() {
+        return `${this._code}, ${this._name}, ${this._symbol}`;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(new_code) {
+        this._code = new_code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(new_name) {
+        this._name = new_name;
+    }
+    
+    get symbol() {
+        return this._symbol;
+    }
+
+    set symbol(new_symbol) {
+        this._symbol = new_symbol;
+    }
+}
+
+
+function fill_currencies() {
+    let tab = Currency.all_currencies;
+
+    // loop through each countries
+    countries.forEach(country => {
+        // if country has no currency, skip this country
+        if (country.currencies === undefined) {
+            return;
+        }
+
+        // loop through each currency of that country
+        country.currencies.forEach(currency => {
+            // if the currency is not already registered
+            if (!Object.keys(tab).includes(currency.code)) {
+               tab[currency.code] = new Currency(currency.code, currency.name, currency.symbol); 
+            }
+        })
+    })
+}
---
+++ html/data/class_language.js
@@ -0,0 +1,48 @@
+class Language {
+    static all_languages = {};
+
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    toString() {
+        return `${this._name} (${this._code})`;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(new_code) {
+        this._code = new_code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(new_name) {
+        this._name = new_name;
+    }
+}
+
+function fill_languages() {
+    let tab = Language.all_languages;
+
+    // loop through each countries
+    countries.forEach((country) => {
+        // if country has no languages, skip this country
+        if (country.languages === undefined) {
+            return;
+        }
+
+        // loop through each language of that country
+        country.languages.forEach((language) => {
+            // if the currency is not already registered            
+            if (!Object.keys(tab).includes(language.iso639_2)) {
+                tab[language.iso639_2] = new Language(language.iso639_2, language.name);
+            }
+        });
+    });
+}
