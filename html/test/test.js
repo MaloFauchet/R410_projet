28859f6bdde36f8ab25c6625a33b2ca81dc0e449
FAUCHET Malo <malofauchetpro@gmail.com>
2025-03-12 16:18:21 +0100

    assigned tests

--- html/test/test.js
+++ html/test/test.js
@@ -1,35 +1,43 @@
 "use strict";
 
+// Malo
 function outsideTheContinent() {
     Country.all_countries.forEach(country => {
         console.log(country);
     });
 }
 
+// Igor
 function moreNeighbors() {
     
 }
 
+// Malo
 function neighborless() {
     
 }
 
+// Igor
 function moreLanguages() {
     
 }
 
+// Malo
 function withCommonLanguage() {
     
 }
 
+// Igor
 function withoutCommonCurrency() {
     
 }
 
+// Malo
 function sortingDecreasingDensity() {
     
 }
 
+// Igor
 function moreTopLevelDomains() {
     
 }
